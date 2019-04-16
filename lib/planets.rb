a0d9043500480b60ad3025e3d4fd193df883f522
Daviicode <d.ydzdavid@yahoo.com>
2019-04-16 10:13:51 -0400

    planet_initial_setup

--- lib/planets.rb
+++ lib/planets.rb
@@ -1,3 +1,18 @@
 class Planet < ActiveRecord::Base
-  
+
+  #creat a helper method to get accesss of all the planet location from api
+  respond = RestClient.get("https://rickandmortyapi.com/api/location/")
+  all_planet_respond = respond.body
+  all_planet = JASON.parse(all_planet_respond)
+
+  def self.select_planet
+    #will be able to selecte a planet with its name
+  end
+
+  def self.go_to_a_random_planet
+    #be able to randomly jump to a planet(user may visited/or never)
+  end
+
+
+
 end
