49749387949de3dccf77464ccbd7716c0ab63208
ferrisboran <ferris@ferryrules.com>
2019-04-16 09:47:31 -0400

    Add Story

--- docs/stories.rb
+++ docs/stories.rb
@@ -1,10 +1,16 @@
 ########## NEW USER STORY ##################
+@morty = "\033[1;33m\ Morty: \033[1;36m\ "
+@beth = "\033[1;31m\ Beth: \033[1;34m\ "
+@jerry = "\033[0;32m\ Jerry: \033[1;30m\ "
+@summer = "\033[1;35m\ Summer: \033[1;37m\ "
+@rick = "\033[1;36m\ Rick: \033[0;33m\ "
+
 def new_user_story(username)
-  puts "\033[1;33m\ Morty: \033[1;36m\ Hey, Rick?"
-  puts "\033[1;31m\ Beth: \033[1;34m\ Hey, Dad?"
-  puts "\033[0;32m\ Jerry: \033[1;30m\ Rick?"
-  puts "\033[1;35m\ Summer: \033[1;37m\ Grandpa Rick?"
-  puts "\033[1;36m\ Rick: \033[0;33m\ I'm going on vacation!"
+  puts "#{@morty}Hey Rick?"
+  puts "#{@beth}Hey, Dad?"
+  puts "#{@jerry}Rick?"
+  puts "#{@summer}Grandpa Rick?"
+  puts "#{@rick}I'm going on vacation!"
 end
 
 
@@ -14,5 +20,7 @@ new_user_story("ferryrules")
 
 ########## RETURNING USER WELCOME ###########
 def returning_user_story(username)
-  puts "Get out of here, Morty! And take #{username} with you..."
+  puts "#{@rick}Get out of here, Morty! And take #{username} with you..."
 end
+
+returning_user_story("ferryrules")
